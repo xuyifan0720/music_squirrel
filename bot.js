1fa9580a1123a00e1246b35538a7510723b52897
Yeefs <xuyifan@Yifans-black-box.local>
2020-08-30 19:40:07 -0400

    changed volume default back

--- bot.js
+++ bot.js
@@ -85,7 +85,7 @@ client.on("message", message => {
                     songQueues.set(message.guild.id, []);
                     modes.set(message.guild.id, ENGLISH);
                     const clientVoice = clientConnection.channel;
-                    VOLUME = 0.5;
+                    VOLUME = 0;
                     play(clientVoice, "https://www.youtube.com/watch?v=7nQ2oiVqKHw", message, 
                         (x) => {VOLUME = 0.5; currDispatchers.set(message.guild.id, null)});
                     console.log("conncted to voice channel")
